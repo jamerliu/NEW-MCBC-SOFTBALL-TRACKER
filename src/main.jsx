37dacb0ce011751600a4663c053a923eac2f7686
jamerliu <jamesliu.07.zl@gmail.com>
2026-06-21 19:26:18 -0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import "./storage.js"; // sets up window.storage before App ever calls it
+import "./index.css"; // Tailwind's generated styles
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
